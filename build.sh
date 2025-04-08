70fbd12b9da82f19af6506b94b0ddb9c94a37e41
Markus W. Mahlberg <markus@mahlberg.io>
2025-04-08 13:00:44 +0200

    refactor: Put the lockfile outside the designated volumes

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # set -e
 # set -o pipefail
 cd /usr/local/src/site
-if [ -f .buildlock ]; then
+if [ -f /tmp/.buildlock ]; then
   echo "Build is already in progress. Exiting."
   exit 0
 fi
-trap "rm -f .buildlock" EXIT
-touch .buildlock
+trap "rm -f /tmp/.buildlock" EXIT
+touch /tmp/.buildlock
 git submodule init && git submodule update --progress && /usr/bin/hugo build --destination /srv
